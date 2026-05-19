63a9bba7ca72665c58795c0b2a02b9f8b6731149
Nicola Franceschinis <franceschinis@logica-hs.com>
2026-05-19 15:18:27 +0200

    Added .venv recreation capabilities

--- systemd_cloud.sh
+++ systemd_cloud.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-source "$(dirname "$0")/.venv/bin/activate" 
-cd "$(dirname "$0")/resources"
 
-python3 de01cloud.py
+if [ ! -d $(dirname "$0")/.venv ]; then
+  python -m venv .venv
+  source "$(dirname "$0")/.venv/bin/activate"
+  pip install --no-index --find-links=$(dirname "$0")/pyPackages/ -r requirements.txt
+
+else
+  source "$(dirname "$0")/.venv/bin/activate" 
+  cd "$(dirname "$0")/resources"
+
+  python3 de01cloud.py
+
+fi
